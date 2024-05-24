8815bfd74b56ad653f2c96470527f323e0d766ff
nsltharaka <nsltharaka@gmail.com>
2024-05-24 19:20:29 +0530

    bug fixes

--- service/post/router.go
+++ service/post/router.go
@@ -38,15 +38,13 @@ func (h *Handler) RegisterRoutes() http.Handler {
 
 func (h *Handler) handleGetAllPosts(w http.ResponseWriter, r *http.Request) {
 	userID := r.Context().Value(auth.ContextKey("authUser")).(int)
+	limit := 20
 
 	q := r.URL.Query()
-	limitStr := q.Get("limit")
-	offsetStr := q.Get("offset")
+	pageStr := q.Get("page")
 
-	limit, limitErr := strconv.Atoi(limitStr)
-	offset, offsetErr := strconv.Atoi(offsetStr)
-
-	if limitErr != nil || offsetErr != nil {
+	page, err := strconv.Atoi(pageStr)
+	if err != nil {
 		utils.WriteError(w, http.StatusBadRequest, errors.New("invalid query params"))
 		return
 	}
@@ -54,13 +52,13 @@ func (h *Handler) handleGetAllPosts(w http.ResponseWriter, r *http.Request) {
 	resultSet, _ := h.db.GetAllTopicsWithLimitAndOffset(r.Context(), database.GetAllTopicsWithLimitAndOffsetParams{
 		UserID: int32(userID),
 		Limit:  int32(limit),
-		Offset: int32(offset),
+		Offset: int32((page - 1) * limit),
 	})
 
 	// next page link if available
 	next := ""
 	if len(resultSet) == limit {
-		next = fmt.Sprintf("/posts/all?limit=%d&offset=%d", limit, (offset + limit))
+		next = fmt.Sprintf("/posts/all?page=%d", page+1)
 	}
 
 	pageInfo := map[string]any{
@@ -73,7 +71,7 @@ func (h *Handler) handleGetAllPosts(w http.ResponseWriter, r *http.Request) {
 		posts = append(posts, utils.PostToPostPayload(row))
 	}
 
-	log.Println("HANDLE_GET_POSTS payload length=", len(posts))
+	log.Println("HANDLE_GET_POSTS nextPage=", next)
 	utils.WriteJSON(w, http.StatusOK, map[string]any{
 		"info":  pageInfo,
 		"posts": posts,
