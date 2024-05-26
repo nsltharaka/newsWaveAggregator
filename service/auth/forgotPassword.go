e65401f9574daa0f38024449597eebd2425adf0d
nsltharaka <nsltharaka@gmail.com>
2024-05-27 00:08:26 +0530

    bug fix

--- service/auth/forgotPassword.go
+++ service/auth/forgotPassword.go
@@ -15,10 +15,6 @@ import (
 	"gopkg.in/gomail.v2"
 )
 
-var (
-	chnCaseDeleteSignal = make(chan struct{})
-)
-
 func (h *Handler) handleForgotPassword(w http.ResponseWriter, r *http.Request) {
 	// get user email from the request
 	// example request body
